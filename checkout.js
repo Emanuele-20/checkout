89697baf690a5af90ad527ebe85bee9b27e34a19
Emanuele Pace <emanuele.10@outlook.it>
2021-02-27 09:23:18 +0000

    Special Offer 2B, tested and solved without hardcoding the logic

--- checkout.js
+++ checkout.js
@@ -2,39 +2,52 @@ class Shop{
 
     constructor(){
         this.value = {
-            "A":50,
-            "B":30,
-            "C":20,
-            "D":15
+            "A": 50,
+            'B': 30,
+            "C": 20,
+            "D": 15
         }
+    }
+
+    numbersOfAs(par){
+        const numbOfAs = par.match(/A/g)
+        if(numbOfAs === null){
+            return 0
+        } 
+        return numbOfAs.length
+    }
+    numbersOfBs(par){
+        const numbOfBs = par.match(/B/g)
+        if(numbOfBs === null){
+            return 0
+        }
+        return numbOfBs.length
     }
 
 
     checkout(val){
-        const valuesAccumulator = []
-        
-        if ((typeof val == 'number') || (val !== val.toUpperCase())){
+
+        let array = []
+
+        if((typeof val === 'number') || (typeof val !== 'string') || (val !== val.toUpperCase())){
             return -1
-        } else if((val.match(/A/g).length) % 3 === 0 ){
-            valuesAccumulator.push(130)
-        } else if (val.match(/B/g).length % 2 === 0 ){
-            valuesAccumulator.push(45)
+        } 
+        if(this.numbersOfAs(val) >= 3){   
+            array.push((Math.floor((this.numbersOfAs(val)/3)) * 130) + ((this.numbersOfAs(val) % 3) * 50 ))// how many extra As after a set of them
+        } else if (this.numbersOfBs(val) >= 2){
+            array.push((Math.floor((this.numbersOfBs(val)/2)) * 45) + ((this.numbersOfBs(val) %2) * 30))
         } else {
             for(let i = 0; i < val.length; i++){
                 let letter = val[i]
-                valuesAccumulator.push(this.value[letter])
+                array.push(this.value[letter])
             }
-        }  
-
+        }
         
-
-        let result = valuesAccumulator.reduce(function(accumulator,currentValue){
-            return accumulator + currentValue;
-        } , 0)
-
+        let result = array.reduce((a, b) => a + b, 0)
+        console.log(result)
         return result
-    };
-
+        
+    }
 }
 
-module.exports = Shop
+module.exports = Shop;
--- checkout.test.js
+++ checkout.test.js
@@ -1,60 +1,56 @@
 const Shop = require('./checkout')
 
-describe("Supermarket Checkout", () => {
-    
-    it('Returns a new instance of the Shop Class', () => {
-        shop = new Shop
-        expect(shop).toBeInstanceOf(Shop);
-    });
 
-    it('Returns -1 when "aBc" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("aBc")).toBe(-1);
-    });
+describe("Checkout" , () => {
+    test("Return -1 if the argument passed is not fully capitalize", () => {
+        shop = new Shop()
+        expect(shop.checkout('aBc')).toBe(-1)
+    })
 
-    it('Returns -1 when "-B8x" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("-B8x")).toBe(-1);
-    });
+    test("Return -1 if the argument passed contain invalid digits", () => {
+        shop = new Shop()
+        expect(shop.checkout('-B8x')).toEqual(-1)
+    })
 
-    it('Returns -1 when an integer is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout(18)).toBe(-1);
-    });
+    test("Return -1 if the argument passed is a number", () => {
+        shop = new Shop()
+        expect(shop.checkout(8)).toEqual(-1)
+    })
 
-    it('Returns 100 when an "AA" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("AA")).toBe(100);
-    });
+    test("Return 100 if 'AA' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("AA")).toEqual(100)
+    })
 
-    it('Returns 20 when an "C" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("C")).toBe(20);
-    });
+    test("Return 115 if 'ABCD' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("ABCD")).toEqual(115)
+    })
+})
 
-    it('Returns 100 when an "ABC" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("ABC")).toBe(100);
-    });
-
-    it('Returns 115 when an "ABCD" is passed as argument', () => {
-        shop = new Shop
-        expect(shop.checkout("ABCD")).toBe(115);
-    });
-});
-
-
-describe("Special Offerts", () => {
-    
-    it('Returns 130 when an "AAA" is passed as argument, Special Offer', () => {
-        shop = new Shop
-        expect(shop.checkout("AAA")).toBe(130);
-    });
-
-
-    it('Returns 45 when an "BB" is passed as argument, Special Offer', () => {
-        shop = new Shop
-        expect(shop.checkout("BB")).toBe(45);
-    });
-
-});
+describe("Special Offer", () => {
+    it("Return 130 when 'AAA' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("AAA")).toEqual(130)
+    })
+    it("Return 260 when 'AAAAAA' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("AAAAAA")).toEqual(260)
+    })
+    it("Return 180 when 'AAAA' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("AAAA")).toEqual(180)
+    })
+    it("Return 230 when 'AAAAA' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("AAAAA")).toEqual(230)
+    })
+    it("Return 45 instead of 60  when 'BB' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("BB")).toEqual(45)
+    })
+    it("Return 75 'BBB' is passed", () => {
+        shop = new Shop()
+        expect(shop.checkout("BBB")).toEqual(75)
+    })
+})
